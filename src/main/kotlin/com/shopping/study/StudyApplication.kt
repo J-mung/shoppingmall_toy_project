004eba65171547df95310e4070ccb257f8fa2f8a
J-mung <xorb2298@gmail.com>
2025-02-28 21:47:03 +0900

    2주차 로그인 화면 추가 및 controller 연결

--- src/main/kotlin/com/shopping/study/StudyApplication.kt
+++ src/main/kotlin/com/shopping/study/StudyApplication.kt
@@ -1,10 +1,9 @@
 package com.shopping.study
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(exclude= [DataSourceAutoConfiguration::class])
+@SpringBootApplication
 class StudyApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/shopping/study/auth/controller/AuthController.kt
+++ src/main/kotlin/com/shopping/study/auth/controller/AuthController.kt
@@ -1,15 +1,27 @@
 package com.shopping.study.auth.controller
 
+import com.shopping.study.auth.dto.UsersDto
+import com.shopping.study.auth.service.AuthService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
-class AuthController {
+class AuthController(
+    private val authService: AuthService
+) {
+    val loginPage: String = "login.html"
 
-    @GetMapping("/great")
+
+    @GetMapping("/login")
     fun getLoginPage(model: Model): String {
-        model.addAttribute("message", "Hello, Kotlin + Spring Boot!")
-        return "login.html"
+        return loginPage
+    }
+
+    @PostMapping("/login")
+    fun requestLogin(@RequestParam(name = "") user_id: String, user_pw: String) {
+        authService.authenticate(user_id, user_pw)
     }
 }
