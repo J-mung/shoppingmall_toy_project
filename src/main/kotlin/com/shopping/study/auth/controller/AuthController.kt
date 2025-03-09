74428157a705c2436bcb823d5cc2a9b0112cdd33
J-mung <xorb2298@gmail.com>
2025-03-09 23:11:26 +0900

    4주차 컨트롤러 url 경로 수정 및 requestLogin 메소드 명칭 변경

--- src/main/kotlin/com/shopping/study/auth/controller/AuthController.kt
+++ src/main/kotlin/com/shopping/study/auth/controller/AuthController.kt
@@ -3,8 +3,7 @@ package com.shopping.study.auth.controller
 import com.shopping.study.auth.dto.loginDto
 import com.shopping.study.auth.dto.logoutDto
 import com.shopping.study.auth.service.AuthService
-import com.shopping.study.util.annotations.AuthCheck
-import com.shopping.study.util.annotations.BeforeAuthCheck
+import com.shopping.study.util.annotations.CheckLoginState
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
@@ -12,8 +11,10 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
+@RequestMapping("/auth")
 class AuthController(
     private val authService: AuthService
 ) {
@@ -23,7 +24,7 @@ class AuthController(
     /**
      * login 페이지 요청
      */
-    @GetMapping("/loginpage")
+    @GetMapping("/login")
     fun getLoginPage(model: Model): String {
         return loginPage
     }
@@ -31,14 +32,13 @@ class AuthController(
     /**
      * login 요청
      */
-    @PostMapping("/login/auth")
-    @BeforeAuthCheck
-    fun requestLogin(@RequestBody loginDto: loginDto, request: HttpServletRequest): ResponseEntity<Any> {
+    @PostMapping("/login")
+    @CheckLoginState
+    fun login(@RequestBody loginDto: loginDto, request: HttpServletRequest): ResponseEntity<Any> {
         return authService.login(loginDto, request)
     }
 
     @PostMapping("/logout")
-    @BeforeAuthCheck
     fun requestLogout(@RequestBody logoutDto: logoutDto, request: HttpServletRequest): ResponseEntity<Map<String, String>> {
         return authService.logout(logoutDto, request)
     }
