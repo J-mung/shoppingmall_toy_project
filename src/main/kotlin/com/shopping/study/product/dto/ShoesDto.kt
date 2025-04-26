936884d15be29ee2d1583a582aa541c85e5ff0d3
J-mung <xorb2298@gmail.com>
2025-04-26 18:36:03 +0900

    #2-상품기능_구현-세부상품 표현을 위한 interface / 신발 객체 생성

---
+++ src/main/kotlin/com/shopping/study/product/dto/ShoesDto.kt
@@ -0,0 +1,21 @@
+package com.shopping.study.product.dto
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import java.io.Serializable
+
+data class ShoesDto(
+    override var id: Int,
+    override var categoryId: Int,
+    override var productName: String,
+    override var price: Int,
+    override var stock: Int?,
+    var size: Int?,
+    var color: String?,
+): ProductInterface, Serializable {
+    fun setDetail(detailJson: String) {
+        val objectMapper = ObjectMapper()
+        val jsonNode = objectMapper.readTree(detailJson)
+        this.size = jsonNode["size"].asInt()
+        this.color = jsonNode["color"].asText()
+    }
+}
