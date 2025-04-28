ecb82fcb4fa34ed6a82999cef77078b57a504ec4
J-mung <xorb2298@gmail.com>
2025-04-28 23:41:25 +0900

    #2-상품기능_구현-상품화면 접속 시, 카테고리 정보 가져오기

--- src/main/kotlin/com/shopping/study/product/controller/ProductController.kt
+++ src/main/kotlin/com/shopping/study/product/controller/ProductController.kt
@@ -1,16 +1,21 @@
 package com.shopping.study.product.controller
 
+import com.shopping.study.category.service.CategoryService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/product")
-class ProductController {
+class ProductController (
+    val categoryService: CategoryService
+) {
     val productHome = "productHome"
 
     @GetMapping("/", "")
-    fun getProductPage(): String {
+    fun getProductPage(model: Model): String {
+        model.addAttribute("categoryList", categoryService.getCategoryList())
         return productHome
     }
 }
