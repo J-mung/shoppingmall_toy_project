6740d0813c723c18463ac40d8704fc7dd0c2eaff
J-mung <xorb2298@gmail.com>
2025-04-29 00:03:57 +0900

    #2-상품기능_구현-ShoesDto에서 detail json을 직접 parsing 하려 했지만, ProductMapperRegistry에 위임함으로써 코드 정리

--- src/main/kotlin/com/shopping/study/product/dto/ShoesDto.kt
+++ src/main/kotlin/com/shopping/study/product/dto/ShoesDto.kt
@@ -11,11 +11,4 @@ data class ShoesDto(
     override var stock: Int?,
     var size: Int = 0,
     var color: String = "",
-): ProductInterface, Serializable {
-    fun setDetail(detailJson: String) {
-        val objectMapper = ObjectMapper()
-        val jsonNode = objectMapper.readTree(detailJson)
-        this.size = jsonNode["size"].asInt()
-        this.color = jsonNode["color"].asText()
-    }
-}
+): ProductInterface, Serializable
