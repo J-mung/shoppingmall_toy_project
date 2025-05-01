7dad52e739230e5ba0bc711fcd3c996e9f3c9c1f
J-mung <xorb2298@gmail.com>
2025-05-01 23:19:56 +0900

    #2-상품기능_구현-cache 전체 변경이면 CacheEvict 적용이 깔금한 코드

--- src/main/kotlin/com/shopping/study/category/service/CategoryService.kt
+++ src/main/kotlin/com/shopping/study/category/service/CategoryService.kt
@@ -3,7 +3,7 @@ package com.shopping.study.category.service
 import com.shopping.study.category.dto.CategoryDto
 import com.shopping.study.category.mapper.CategoryMapper
 import com.shopping.study.category.repository.CategoryRepository
-import org.springframework.cache.annotation.CachePut
+import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.jpa.domain.AbstractPersistable_.id
 import org.springframework.stereotype.Service
@@ -18,7 +18,7 @@ class CategoryService (
      * 카테고리 리스트 조회
      */
     @Transactional(readOnly = true)
-    @Cacheable(value = ["category"], key = "'all'")
+    @Cacheable(value = ["category"])
     fun getCategoryList(): List<CategoryDto> {
         val categoryEntity = categoryRepository.findAll()
         return categoryEntity.map {
@@ -29,8 +29,8 @@ class CategoryService (
     /**
      * 카테고리 변경
      */
-    @CachePut(value = ["category"], key = "'all'")
-    fun updateCategory(categoryDto: CategoryDto): List<CategoryDto> {
+    @CacheEvict(value = ["category"], allEntries = true)
+    fun updateCategory(categoryDto: CategoryDto) {
         val categoryEntity = categoryRepository.findById(categoryDto.categoryId)
             .orElseThrow {
                 NoSuchElementException("Entity with id $id not found")
@@ -42,9 +42,5 @@ class CategoryService (
 
         categoryEntity.categoryName = categoryDto.categoryName
         categoryRepository.save(categoryEntity)
-
-        // categoryDto 리스트를 새로 덮어야 하기 때문에 전체 조회
-        val categoryEntityList = categoryRepository.findAll()
-        return categoryEntityList.map { categoryMapper.toDto(it) }
     }
 }
