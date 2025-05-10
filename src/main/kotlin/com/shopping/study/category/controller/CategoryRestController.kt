125f971658321b7b7810e905a4b52637d5d15f88
J-mung <xorb2298@gmail.com>
2025-05-10 15:37:32 +0900

    #2-상품기능_구현-개선된 categoryService 적용

--- src/main/kotlin/com/shopping/study/category/controller/CategoryRestController.kt
+++ src/main/kotlin/com/shopping/study/category/controller/CategoryRestController.kt
@@ -2,6 +2,9 @@ package com.shopping.study.category.controller
 
 import com.shopping.study.category.dto.CategoryDto
 import com.shopping.study.category.service.CategoryService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,8 +15,33 @@ import org.springframework.web.bind.annotation.RestController
 class CategoryRestController(
     val categoryService: CategoryService
 ) {
-    @PostMapping("/update")
-    fun updateOneCategory(@RequestBody categoryDto: CategoryDto) {
-        categoryService.updateCategory(categoryDto)
+    @GetMapping("/list")
+    fun getCategoryList(): ResponseEntity<List<CategoryDto>> {
+        return ResponseEntity.ok().body(
+            categoryService.getCategoryList()
+        )
     }
+
+    @GetMapping("/{categoryId}")
+    fun getOneCategory(@PathVariable categoryId: Int): ResponseEntity<CategoryDto> {
+        categoryService.getOneCategory(categoryId)
+        return ResponseEntity.ok().body(
+            categoryService.getOneCategory(categoryId)
+        )
+    }
+
+    @PostMapping("/update")
+    fun updateCategory(@RequestBody categoryDto: CategoryDto): ResponseEntity<Boolean> {
+        return ResponseEntity.ok().body(
+            categoryService.updateCategory(categoryDto)
+        )
+    }
+
+    @PostMapping("/single/update")
+    fun updateOneCategory(@RequestBody categoryDto: CategoryDto): ResponseEntity<CategoryDto> {
+        return ResponseEntity.ok().body(
+            categoryService.updateOneCategory(categoryDto)
+        )
+    }
+
 }
